669ac1d3e4186d35cd38d3164af82775988d44c0
sainiftw <ankitsaini200@gmail.com>
2014-03-13 20:37:24 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$app_id = "305108979577826";
+$app_secret = "d0c3556f617af07a48a26b8353f029bb";
+$canvasPage = "http://apps.facebook.com/_crush_calculation";
+
+
+
+$scripturl = "";
+
+$like = "";
+
+$spam="Check how many people have Crush on you @ http://apps.facebook.com/_crush_calculation";
+$albumdesc="";//album description
+$albumname = "My Crushes";
+?>
